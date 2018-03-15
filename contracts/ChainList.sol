c621ad5c5607096ea7078fa434c42b0436d62c7e
soudegesu <soudegesu@hotmail.co.jp>
2018-03-16 07:34:49 +0900

    add inheritence

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.18;
 
-contract ChainList {
+import "./Ownable.sol";
+
+contract ChainList is Ownable {
   // custom types
   struct Article {
     uint id;
@@ -12,7 +14,6 @@ contract ChainList {
   }
 
   // state variables
-  address owner;
   mapping (uint => Article) public articles;
   uint articleCounter;
 
@@ -30,17 +31,6 @@ contract ChainList {
     string _name,
     uint256 _price
   );
-
-  // modifiers
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  // constructor
-  function ChainList() public {
-    owner = msg.sender;
-  }
 
   // deactivate the contract
   function kill() public onlyOwner {
